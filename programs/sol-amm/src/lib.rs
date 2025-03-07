c52d1d8d6a78160d0fba0292e52f215ebaaba7fb
Udoka_AM <udoka_eth@gmail.com>
2025-03-07 23:57:48 +0100

    add new

--- programs/sol-amm/src/lib.rs
+++ programs/sol-amm/src/lib.rs
@@ -1,16 +1,30 @@
 use anchor_lang::prelude::*;
 
+mod errors;
+mod state;
+mod instruction;
+
+use instruction::*;
+
 declare_id!("6mgv17dd75FTsfR2an6F6VYdRnKzfiVkPw964P2TQYmA");
 
 #[program]
-pub mod sol_amm {
+pub mod amm {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize(ctx: Context<Initialize>, seed: u64, fee: u16, authority: Option<Pubkey>) -> Result<()> {
+        ctx.accounts.init(seed, fee, authority, ctx.bumps)
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y)
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount, max_x, max_y)
+    }
+
+    pub fn swap(ctx: Context<Swap>, is_x: bool, amount_in: u64, min_amount_out: u64) -> Result<()> {
+        ctx.accounts.swap(is_x, amount_in, min_amount_out)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
